2b4b94a25154108025965e22d99afbdfb8baa8cb
Nils Riedemann <nils.riedemann@gmail.com>
2012-06-08 13:41:41 +0200

    full block parsing

--- rewritem.rb
+++ rewritem.rb
@@ -1,8 +1,13 @@
 class Rewritem
   def self.parse filepath
     result = ""
+
     file = File.read(filepath)
-    result = file.scan( /^\ \d.*$/m )
-    result[0]
+    file.each_line do |line|
+      _result = line.match( /^\ \d.*$/m ).to_s
+      result << _result unless _result.nil?
+    end
+
+    result
   end
 end
--- test/rewritem_test.rb
+++ test/rewritem_test.rb
@@ -2,9 +2,16 @@ require 'test/unit'
 require File.expand_path('../../rewritem.rb', __FILE__)
 
 class RewritemTest < Test::Unit::TestCase
-  def test_parsing_example_file
+  def test_parsing_small_example_file
     sample_result = File.read("test/assets/small_example_result.txt")
     sample = Rewritem.parse "test/assets/small_example.dat" 
+
+    assert_equal(sample_result, sample)
+  end
+
+  def test_parsing_full_example_file
+    sample_result = File.read("test/assets/full_example.txt")
+    sample = Rewritem.parse "test/assets/full_example.dat" 
 
     assert_equal(sample_result, sample)
   end
